a871d90bd643dd1aa13fce64efb685518b861848
Himashi Karunathilake <himashikarunathilake@gmail.com>
2023-08-09 22:25:17 +0530

    Part 1 completed.

---
+++ Part-1/main.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# The first program in Perl.
+
+# Pragmas
+use strict;
+use warnings;
+use feature 'say';
+
+# Print a single line
+print "Hello World!";
+
+# Print an empty line using the print function
+print "\n\n";
+
+# Declare variables to store a string and a number
+my $name = "Dwight";
+my $age = 30;
+my $module = "The Office";
+
+# Print the variables using the print function
+print "Name of the programmer: $name\n";
+print "Age of the programmer: $age\n";
+
+# Print an empty line using the say function
+say "";
+
+# Print the variables using the say function
+say "Name of the module: $module";
